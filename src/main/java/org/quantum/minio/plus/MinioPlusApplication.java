860c05f061413d122309bd97e1d388edeab4d9b2
蚍蜉 <jpx1024@gmail.com>
2021-03-29 16:30:55 +0800

    初始化工程

---
+++ src/main/java/org/quantum/minio/plus/MinioPlusApplication.java
@@ -0,0 +1,13 @@
+package org.quantum.minio.plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinioPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinioPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/quantum/minio/plus/MinioPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.quantum.minio.plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinioPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
